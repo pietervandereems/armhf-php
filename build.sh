34ce79498829837f3a086c8d1aa9f2c953ef1d3b
Pieter van der Eems <github@eemco.nl>
2016-12-26 14:07:11 +0100

    Bugfix in build, typo, remove double ::

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 DOCKER='/usr/bin/docker'
 
-${DOCKER} build -f Dockerfile.alpine.php7 -t pietervandereems/armhf-php::7.0-fpm-alpine .
-echo To Push: ${DOCKER} push pietervandereems/armhf-php::7.0-fpm-alpine
+${DOCKER} build -f Dockerfile.alpine.php7 -t pietervandereems/armhf-php:7.0-fpm-alpine .
+echo To Push: ${DOCKER} push pietervandereems/armhf-php:7.0-fpm-alpine
